685d95cbecbca47038791b419331274c75a93dc0
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2024-05-28 11:07:51 -0400

    = Adding back css neeeded for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,3 +48,106 @@ export let seo;
     </div>
   </div>
 </div>
+
+<style>
+@media screen and (min-width: 900px) {
+  .subpage {
+    position: fixed;
+    height: 100vh;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    padding-top: 3.75rem;
+  }
+}
+
+.subpage__inner {
+  width: 100%;
+}
+
+@media screen and (min-width: 900px) {
+  .subpage__inner .subpage__left-content {
+    width: 55%;
+    padding-right: var(--level1);
+    padding-left: var(--level1);
+    overflow: hidden;
+    height: 100vh;
+    overflow: auto;
+    position: relative;
+    overflow-x: hidden;
+    background-color: white;
+    z-index: 1;
+    position: relative;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  .subpage__inner .subpage__left-content {
+    width: 55%;
+    padding-right: var(--level3);
+    padding-left: var(--level3);
+  }
+}
+
+.subpage__content-body {
+  padding-top: var(--level4);
+}
+
+@media screen and (min-width: 900px) {
+  .subpage__content-body {
+    padding-top: var(--level7);
+  }
+}
+
+/*
+  
+  @media screen and (min-width: 1400px) {
+    .subpage__content-body {
+      padding-right: var(--level8);
+      padding-left: var(--level8);
+    }
+  } */
+
+.subpage__right-content {
+  display: none;
+}
+
+@media screen and (min-width: 900px) {
+  .subpage__right-content {
+    background-color: black;
+    height: 100vh;
+    display: flex;
+    width: 40%;
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
+}
+
+@media screen and (min-width: 1400px) {
+  .subpage__right-content {
+    width: 50%;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  .subpage__right-content {
+    width: 60%;
+    padding-right: 1rem;
+    padding-left: 1rem;
+    overflow: hidden;
+    height: 100vh;
+    overflow: auto;
+    background-color: black;
+  }
+}
+
+@media screen and (min-width: 1400px) {
+  .subpage__right-content {
+    padding-right: 1.5rem;
+    padding-left: 1.5rem;
+    width: 45%;
+    overflow: auto;
+  }
+}
+</style>
