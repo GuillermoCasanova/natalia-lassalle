2c317468b1a5d3f232bd0dba0d71a7bd45e6c9b5
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2025-05-28 00:32:20 -0400

    + removing /build dir

--- build/server/chunks/error.svelte-CTM0YC_K.js
+++
@@ -1,7 +0,0 @@
-import { x, f as d, e as n } from './exports-WEytBQE0.js';
-import { r } from './stores-i0QuXd1Z.js';
-
-const o=x(((s,o,a,p)=>{let m,n$1;return n$1=n(r,(s=>m=s)),n$1(),`<h1>${d(m.status)}</h1> <p>${d(m.error?.message)}</p>`}));
-
-export { o as default };
-//# sourceMappingURL=error.svelte-CTM0YC_K.js.map
--- build/server/chunks/stores-i0QuXd1Z.js
+++
@@ -1,6 +0,0 @@
-import { l, D } from './exports-WEytBQE0.js';
-
-function e(s,t=JSON.parse){try{return t(sessionStorage[s])}catch{}}(D.toString().includes("$$")||/function \w+\(\) \{\}/.test(D.toString()))&&new URL("https://example.com"),e("sveltekit:scroll"),e("sveltekit:snapshot");const r={subscribe:s=>(()=>{const s=l("__svelte__");return {page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}})().page.subscribe(s)};
-
-export { r };
-//# sourceMappingURL=stores-i0QuXd1Z.js.map
